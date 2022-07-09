9c3a896b8bed8003373812c7361a560bb411ba39
aloony <npp3rzwi@gmail.com>
2022-07-09 23:12:17 +0200

    fixtures improved

--- _generate_fixtures.py
+++ _generate_fixtures.py
@@ -2,6 +2,9 @@
 # and then "python _generate_fixtures.py && python manage.py loaddata ./fixtures/data.json" 
 
 import random, string, json
+from faker import Faker
+
+faker = Faker()
 
 with open('fixtures/data.json', 'w') as file:
     data = []
@@ -10,13 +13,9 @@ with open('fixtures/data.json', 'w') as file:
             'model': 'mainapp.Movie',
             'fields': {
                 'imdb_title_id': i,
-                'title': ''.join(random.choice(string.ascii_letters) for _ in range(5)) + ' ' + ''
-                            .join(random.choice(string.ascii_letters) for _ in range(3)) + ' ' + ''
-                            .join(random.choice(string.ascii_letters) for _ in range(8)),
-                'description': ''.join(random.choice(string.ascii_letters) for _ in range(15)) + ' ' + ''
-                                .join(random.choice(string.ascii_letters) for _ in range(5)) + ' ' + ''
-                                .join(random.choice(string.ascii_letters) for _ in range(10)),
-                'director': ''.join(random.choice(string.ascii_letters) for _ in range(8))
+                'title': faker.name(),
+                'description': faker.text(),
+                'director': faker.name()
             }
         })
     file.write(json.dumps(data))
